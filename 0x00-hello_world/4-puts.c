9b2034364668200bd48326eb588f02eed293c9b1
Villaina98 <yongzcreative@gmail.com>
2023-02-16 19:12:45 -0800

    4-puts.c script fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - A program that uses the function puts to print a specific line followed by a new line
+ * main - A program that uses the function puts to print a specific line
  * Return: 0
  */
 int main(void)
 {
-puts('\"Programming is like building a multilingual puzzle');
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
+
