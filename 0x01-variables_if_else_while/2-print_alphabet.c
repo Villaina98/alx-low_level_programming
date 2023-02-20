36ee28141f735300092f4e0c3bf4e99557e5b43d
Villaina98 <yongzcreative@gmail.com>
2023-02-20 08:16:24 -0800

    2-print_alphabet.c first commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in lowercase
+ * Return: 0(success)
+ */
+
+int main(void)
+{
+	int s = 97, e = 122;
+
+	while (s <= e)
+	{
+		putchar(s);
+		s++;
+	}
+	putchar('\n');
+	return (0);
+}
