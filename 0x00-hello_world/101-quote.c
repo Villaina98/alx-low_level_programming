40b9f0dc2af4ac1cb83441e52ee1c61f53141141
Villaina98 <yongzcreative@gmail.com>
2023-02-20 19:43:21 -0800

    101-quote.c first commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints string to the standard error
+ * Record: 1 (success)
+ */
+
+int main(void)
+{
+	system("echo 'and that piece of art is useful\" \- Dora Korpar, 2015-10-19'");
+	return (1);
+}
