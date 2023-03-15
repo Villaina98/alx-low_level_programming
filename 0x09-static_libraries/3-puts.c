6aed5afdb0df956c5bf33eb3fd7bff4990dcaed7
Villaina98 <yongzcreative@gmail.com>
2023-03-15 10:47:00 -0700

    fixing create_static_lib.sh commit

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,14 +1,11 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _puts - Entry point
+ * _puts - main entry
  * @s: character
- * Return: Always 0 (Success)
+ *
  */
 
 void _puts(char *s)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
 }
